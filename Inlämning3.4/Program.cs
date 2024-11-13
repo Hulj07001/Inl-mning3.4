972e1d8af010828b19a2cac1530b5d903b24b5c5
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-11-13 12:45:57 +0100

    Add project files.

---
+++ Inlämning3.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace inlämning3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej och välkommen till vår Radiostation!");
+            Console.WriteLine("Vi har några krav om din önskelåt ska spelas.");
+            Console.WriteLine("Hur lång är din låt? Svara i minuter och sekunder.");
+            Console.Write("Minuter: ");
+            int minut = int.Parse(Console.ReadLine());
+            Console.Write("Sekunder: ");
+            int sekund = int.Parse(Console.ReadLine());
+
+            int totalsekund = minut * 60 + sekund;
+            int minst = 2 * 60 + 45;
+            int max = 4 * 60 + 20;
+
+            if (totalsekund >= minst && totalsekund <= max)
+            {
+                Console.WriteLine("Din låt när våra krav!");
+            }
+            else
+            {
+                Console.WriteLine("Din låt når inte våra krav");
+            }
+        }
+    }
+}
